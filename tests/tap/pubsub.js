c66514205c52fedc432bbe0e3b608999ca3b5db3
Tommy Messbauer <tommy@vast.com>
2013-05-23 13:45:08 -0500

    added error event

--- tests/tap/pubsub.js
+++ tests/tap/pubsub.js
@@ -1,28 +1,28 @@
-var fixture = require('../fixtures.js');
 var Splurge = require('../../index.js');
 var tap = require('tap');
 var test = tap.test;
 
-fixture(function(err, hub) {
-
-  if (err) {
-    throw err;
-  }
-
-  test('Client connect', function(t) {
-    var client = new Splurge.Client({ port: hub.port });
-
-    client.connect(function(err) {
-
-      t.notOk(err, 'Should not return error');
-      t.ok(client._remote, 'Remote functions should exist');
-      t.ok(client._remote.publish, 'Publish should exist');
-      t.type(client._remote.publish, 'function', 'Publish should be function');
-      t.ok(client._remote.subscribe, 'Subscribe should exist');
-      t.type(client._remote.subscribe, 'function', 'Subscribe should be function');
-      t.end();
-    });
+test('Client connect', function(t) {
+  var client = new Splurge.Client({ 
+    host: 'fedev5.vast.com',
+    port: 8989
   });
 
+  client.connect(function(err) {
+
+    t.notOk(err, 'Should not return error');
+    t.ok(client._remote, 'Remote functions should exist');
+    t.ok(client._remote.publish, 'Publish should exist');
+    t.type(client._remote.publish, 'function', 'Publish should be function');
+    t.ok(client._remote.subscribe, 'Subscribe should exist');
+    t.type(client._remote.subscribe, 'function', 'Subscribe should be function');
+
+    client.subscribe('foo', function(subErr) {
+      t.notOk(subErr, 'Should not return error on subscribe');
+      t.end();
+    });
+
+  });
 });
+
 
